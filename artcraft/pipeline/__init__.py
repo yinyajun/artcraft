3ea5744c88c2f35119aef41bcdbb669acc11a207
yinyajun-hj <yinyajun-hj@huafang.com>
2023-11-09 11:03:15 +0800

    feat: add ip-adapter

--- artcraft/pipeline/__init__.py
+++ artcraft/pipeline/__init__.py
@@ -1,2 +1,3 @@
-from .img2img import image2image
-from .text2img import text2image
+from .img2img import image2image, Image2Image
+from .text2img import text2image, Text2Image
+from .image_adapt import Adapter
